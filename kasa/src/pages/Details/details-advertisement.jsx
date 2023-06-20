1d937c3bd85418e58dd8aaa0dc5bdec326da7a45
Lynerah <celinedeboeck@gmail.com>
2023-06-20 12:36:53 +0200

    create rout for details product page

--- kasa/src/pages/Details/details-advertisement.jsx
+++ kasa/src/pages/Details/details-advertisement.jsx
@@ -1,9 +1,22 @@
-function advertisement () {
-    return (
-        <div>
-            <h1>advertisement </h1>
-        </div>
-    )
-}
+import React from 'react';
+import Carousel from '../components/Carousel';
+import Accordion from '../components/Accordion';
+import { useParams } from 'react-router-dom';
+ import cardData from '../data.json';
 
-export default advertisement 
+
+function ProductDetail() {
+    const { id } = useParams(); // Utilisez le hook useParams pour obtenir la valeur de l'ID du produit
+  
+    // Utilisez l'ID pour récupérer les détails du produit à partir de votre source de données (par exemple, `cardData`)
+  
+    return (
+      <div>
+        <h2>Détails du produit {id}</h2>
+        {/* Affichez les détails du produit */}
+      </div>
+    );
+  }
+  
+  export default ProductDetail;
+  
