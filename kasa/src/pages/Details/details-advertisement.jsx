76affdb9947605a0dcb8573afecb15b3fcc0f9e0
Lynerah <celinedeboeck@gmail.com>
2023-06-21 11:45:01 +0200

    add host info + rating systeme

--- kasa/src/pages/Details/details-advertisement.jsx
+++ kasa/src/pages/Details/details-advertisement.jsx
@@ -5,25 +5,54 @@ import { useParams } from 'react-router-dom';
  import cardData from '../../data.json';
  import '../../style/tag.css';
   import '../../style/accordion-product.css';
+  import '../../style/product-page.css';
 
-
-function ProductDetail() {
-    const { id } = useParams(); // Utilisez le hook useParams pour obtenir la valeur de l'ID du produit
+  function renderRatingStars(rating) {
+    const stars = [];
+    for (let i = 1; i <= 5; i++) {
+      if (i <= rating) {
+        stars.push(<span key={i} className="star filled"></span>);
+      } else {
+        stars.push(<span key={i} className="star"></span>);
+      }
+    }
+    return stars;
+  }
   
+  
+  function ProductDetail() {
+    const { id } = useParams(); // Utilisez le hook useParams pour obtenir la valeur de l'ID du produit
+    
     const product = cardData.find((card) => card.id === id);
+    const rating = product.rating;
 
     if (!product) {
       return <div>Produit non trouvé.</div>;
     }
   
-    const { title,location, tags, pictures, description, equipments } = product;
+    const { title, location, tags, pictures, description, equipments, host } = product;
   
     return (
       <div>
   
         <Carousel pictures={pictures} />
-        <h2 className='product_title'>{title}</h2>
-        <p className='product_location'>{location}</p>
+
+        <div className='product-info_container'>
+          <div className='product-info'>
+            <h2 className='product_title'>{title}</h2>
+            <p className='product_location'>{location}</p>
+          </div>
+          <div className='host_container'>
+            <div className='host_info'>
+
+            <p className='host_name'>{host.name}</p>
+            <img className='host_img' src= {host.picture} alt="" />
+            </div>
+            <div className="rating">
+              {renderRatingStars(rating)}
+            </div>
+          </div>
+        </div>
 
         <div>
             <ul className='tag_list'>
